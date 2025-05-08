6c6dd2fd97056594ba3ec87aa2b0286fac247d67
hiruniwithanagamage <hirunihansika625@gmail.com>
2025-05-08 15:47:01 +0530

    Name Changed - Minor

--- views/member/memberProfile.php
+++ views/member/memberProfile.php
@@ -205,7 +205,7 @@ if (!empty($memberData['Image'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Member Profile - එක්සත් මරණාධාර සමිතිය</title>
+    <title>Member Profile</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <style>
